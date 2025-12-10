18380c3343ce03a9ad0ad430548a8c0bd42e88aa
lucky <luckycatxk@gmail.com>
2025-12-10 19:16:42 +0800

    refactor: optimize

--- safe.go
+++ safe.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"runtime"
+	"strconv"
 )
 
 var ErrPanic = errors.New("panic recovered")
@@ -14,30 +15,34 @@ const bufSize int = 64 << 10
 
 func RecoverContext(ctx context.Context, err *error) {
 	if r := recover(); r != nil {
-		panicAttrs := []slog.Attr{
-			slog.String("type", fmt.Sprintf("%T", r)),
-			slog.Any("value", r),
-		}
+		panicAttrs, locAttrs := make([]slog.Attr, 0, 3), make([]slog.Attr, 0, 3)
+		panicAttrs = append(panicAttrs, slog.String("type", fmt.Sprintf("%T", r)), slog.Any("value", r))
 		var loc string
 		if pc, file, line, ok := runtime.Caller(2); ok {
-			loc = fmt.Sprintf("%s:%d", file, line)
-			locAttrs := []slog.Attr{
-				slog.String("file", file),
-				slog.Int("line", line),
-			}
+			loc = file + ":" + strconv.Itoa(line)
+			locAttrs = append(locAttrs, slog.String("file", file), slog.Int("line", line))
 			if fn := runtime.FuncForPC(pc); fn != nil {
-				loc += " " + fn.Name()
-				locAttrs = append(locAttrs, slog.String("func", fn.Name()))
+				fnName := fn.Name()
+				loc += " " + fnName
+				locAttrs = append(locAttrs, slog.String("func", fnName))
 			}
 			panicAttrs = append(panicAttrs, slog.GroupAttrs("location", locAttrs...))
 		}
 		buf := make([]byte, bufSize)
 		buf = buf[:runtime.Stack(buf, false)]
 		slog.LogAttrs(ctx, slog.LevelError, ErrPanic.Error(), slog.GroupAttrs("panic", panicAttrs...), slog.String("stack", string(buf)))
-		if _, ok := r.(error); !ok {
-			r = fmt.Errorf("%v", r)
+
+		var panicErr error
+		if e, ok := r.(error); ok {
+			panicErr = e
+		} else {
+			panicErr = fmt.Errorf("%v", r)
 		}
-		*err = fmt.Errorf("%w at %s: %w", ErrPanic, loc, r.(error))
+		if loc != "" {
+			*err = fmt.Errorf("%w at %s: %w", ErrPanic, loc, panicErr)
+		} else {
+			*err = fmt.Errorf("%w: %w", ErrPanic, panicErr)
+		}
 	}
 }
 
